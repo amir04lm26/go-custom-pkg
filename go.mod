c8351b0666a335c32f4f691659b47e7041e7a414
Amir <z.amir04lm26@gmail.com>
2024-12-14 21:41:12 +0330

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/amir04lm26/package-example
+
+go 1.23.0
---
+++ package-example.go
@@ -0,0 +1,7 @@
+package mpackage
+
+import "fmt"
+
+func Greet(name string) string {
+	return fmt.Sprintf("Hello, %s", name)
+}
